93f8e9acb6d287ac1fdd3d5924954e3cae9b8ef6
Your Name <you@example.com>
2014-11-03 16:19:29 +0100

    initial commit'

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+
+
+#include <iostream>
+#include <cassert>
+#include <cstdlib>
+
+int main(int argc, char** argv) {
+    std::cout << "Projet de visualisation, viens on est bien bien bien bien bien !!!" << std::endl;    
+    return EXIT_SUCCESS;
+}
