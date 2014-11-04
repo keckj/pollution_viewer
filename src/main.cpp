753e5b0326513bd8010f313d83747ba719d95dc5
Keck Jean-Baptiste <jbkeck@hotmail.com>
2014-11-04 22:29:08 +0100

    changed logs expression

--- src/main.cpp
+++ src/main.cpp
@@ -8,15 +8,16 @@
 
 int main(int argc, char** argv) {
 
+    using log4cpp::log_console;
     log4cpp::initLogs();
 
     std::cout << "Projet de visualisation, viens on est bien bien bien bien bien !!!" << std::endl;    
 
-    log4cpp::log_console->critStream() << "Bien bien bien !";
-    log4cpp::log_console->errorStream() << "Bien bien bien !";
-    log4cpp::log_console->warnStream() << "Bien bien bien !";
-    log4cpp::log_console->infoStream() << "Bien bien bien !";
-    log4cpp::log_console->debugStream() << "Bien bien bien !";
+    log_console->critStream() << "Bien bien bien !";
+    log_console->errorStream() << "Bien bien bien !";
+    log_console->warnStream() << "Bien bien bien !";
+    log_console->infoStream() << "Bien bien bien !";
+    log_console->debugStream() << "Bien bien bien !";
 
     return EXIT_SUCCESS;
 }
