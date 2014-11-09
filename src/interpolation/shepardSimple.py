06e728dabe830e27ec8bd1dbb107a73315f72819
alex <alex.ribard@hotmail.fr>
2014-11-09 17:31:07 +0100

    shepard's simple interoplation seems to be working correctly --aditional testing may be required, optimization are needed

--- src/interpolation/shepardSimple.py
+++ src/interpolation/shepardSimple.py
@@ -12,6 +12,9 @@ from PySide import QtGui
 #	by interpolating specified values
 #=================================#
 
+# weigth computation module
+import weightFunction
+
 # generating qt window
 fig = plt.figure()
 ax = fig.add_subplot(111)
@@ -34,14 +37,39 @@ density = np.zeros((NX,NY))
 
 # we now need to compute the density value in point [mesh_x[i],mesh_y[i]]
 # this will be done via Shepard's methode
+# F(x) = sum(i=1,n) w_i(x) f_i , where w_i(x) : Shepard's weights f_i data
+
+# data generation
+n = 3 # data length
+f = np.random.random_sample((n,)) # generating random data between 0 and 1
+# postion of where the density is known
+x_known = np.random.random_sample((n,))
+y_known = np.random.random_sample((n,))
+knownPos = []
+for i in range(n):
+	knownPos.append(np.array([x_known[i],y_known[i]]))
+
+
+# interpolating
+for i in range(NX):
+	for j in range(NY):
+		x_vec = np.array([mesh_x[i],mesh_y[j]]) 
+		F_x = 0
+		for k in range(n):
+			w = weightFunction.shepardSimpleWeights(k,x_vec,knownPos)
+			F_x += w * f[k]
+
+		density[i,j] = F_x
 
 
 
 # customasing and ploting options
 plt.pcolormesh(mesh_x,mesh_y,density, shading='flat')
+
 plt.title("Shepard simple interpolation")
 plt.axis('image')
 plt.draw()
+plt.colorbar()
 if 'qt' in plt.get_backend().lower():
   QtGui.qApp.processEvents()
 
